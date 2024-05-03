f04d04a8824e405fbb96111e7589e617d55e6da4
Hafizasad07 <hafizasadrafique@gmail.com>
2024-05-03 16:50:47 +0500

    add

--- index-d.php
+++ index-d.php
@@ -8,8 +8,19 @@
     <link rel="stylesheet" href="assets/css/style.css">
     <title>VSL Page</title>
 </head>
-<body>
+<body> 
     <div class="wrapper">
+        <div class="section1">
+            <div class="heading pb-4">Upgrade Your Car, Your Home, And Your <br> Credit Score In 2024, By Doing This <br> Simple 5-Min Trick Once Daily…</div>
+            <div class="pb-4 d-flex justify-content-center">
+                <script type="text/javascript">(function(v,i,d,a,l,y,t,c,s){y='_'+d.toLowerCase();c=d+'L';if(!v[d]){v[d]={};}if(!v[c]){v[c]={};}if(!v[y]){v[y]={};}var vl='Loader',vli=v[y][vl],vsl=v[c][vl+'Script'],vlf=v[c][vl+'Loaded'],ve='Embed';if(!vsl){vsl=function(u,cb){if(t){cb();return;}s=i.createElement("script");s.type="text/javascript";s.async=1;s.src=u;if(s.readyState){s.onreadystatechange=function(){if(s.readyState==="loaded"||s.readyState=="complete"){s.onreadystatechange=null;vlf=1;cb();}};}else{s.onload=function(){vlf=1;cb();};}i.getElementsByTagName("head")[0].appendChild(s);};}vsl(l+'loader.min.js',function(){if(!vli){var vlc=v[c][vl];vli=new vlc();}vli.loadScript(l+'player.min.js',function(){var vec=v[d][ve];t=new vec();t.run(a);});});})(window,document,'Vidalytics','vidalytics_embed_NVghPqQ7aWYVodsw','https://quick.vidalytics.com/embeds/XCINKccT/NVghPqQ7aWYVodsw/');</script>
+                <div id="vidalytics_embed_NVghPqQ7aWYVodsw" class="video" style="width: 50%;  position:relative; padding-top: 300px;"></div>
+            </div>
+        </div>
+        <div class="raise-btn text-center py-5">
+            <a href=""><p class="title1">>> Raise My Scores <<</p><p class="font-14">Click Here to Unlock an 80% Discount Plus Free Bonuses</p><p class="font-12">(Limited-Time Offer)</p></a>
+        </div>
+        <div class="raise-img text-center"><img src="assets/images/raise-img.png" alt="" class="w-50"></div>
         
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
