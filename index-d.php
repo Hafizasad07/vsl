ae754960c91996a8bc6e936e200c6877145fcb18
Hafizasad07 <hafizasadrafique@gmail.com>
2024-05-03 18:32:05 +0500

    new update

--- index-d.php
+++ index-d.php
@@ -20,8 +20,32 @@
         <div class="raise-btn text-center py-5">
             <a href=""><p class="title1">>> Raise My Scores <<</p><p class="font-14">Click Here to Unlock an 80% Discount Plus Free Bonuses</p><p class="font-12">(Limited-Time Offer)</p></a>
         </div>
-        <div class="raise-img text-center"><img src="assets/images/raise-img.png" alt="" class="w-50"></div>
-        
+        <div class="raise-img text-center"><img src="assets/images/raise-img.png" alt="" class=""></div>
+        <div class="container">
+            <div class="font-size-38 pt-3 pb-4">Here’s What You Will Discover Inside The <br> Credit Secrets Program…</div>
+            <div class="credit-secrets-ul">
+                <ul>
+                    <li class="fw-bold pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"> Little-known strategies for removing inquiries from your credit report to boost your scores <br> faster</li>
+                    <li class="pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"> What to do when you want to get personal information removed and the credit bureau is giving <br> you a hard time</li>
+                    <li class="fw-bold pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"> The top 5 credit violations you can take advantage of to turn the tables around in your favor</li>
+                    <li class="pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"> How to remove inaccurate, incomplete, unfair, or unverifiable items from your credit report</li>
+                    <li class="fw-bold pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"> Advanced tactics for handling issues like tax liens, student loans, foreclosures, <br> repossessions, medical collections, and bankruptciesr</li>
+                    <li class="pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"> The “security freeze trick” for removing public records from your credit report</li>
+                    <li class="fw-bold pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"> In-depth insights into data miners and their impact on your credit</li>
+                    <li class="pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"> A little-known “loophole” for removing liens and judgments</li>
+                    <li class="fw-bold pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"> How to deal with identity theft situations and lawsuits</li>
+                    <li class="pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"> What you need to know about ChexSystems removals to raise your scores</li>
+                    <li class="fw-bold pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"> Advanced credit techniques for securing credit lines even with low scores or ZERO credit</li>
+                    <li class="pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"> Post-credit repair steps to maintain high credit scores (so your scores stay high and you never <br> have to experience the frustration, stress, and anxiety that comes with low credit scores ever <br> again)</li>
+                    <li class="fw-bold pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"> And more!</li>
+                </ul>
+            </div>
+            <div class="raise-btn text-center pt-3 pb-5">
+                <a href=""><p class="title1">>> Raise My Scores <<</p><p class="font-14">Click Here to Unlock an 80% Discount Plus Free Bonuses</p><p class="font-12">(Limited-Time Offer)</p></a>
+            </div>
+            <hr>
+            <div class="font-size-38 text-center pt-4">Plus, Get Instant Access To These FREE Bonuses…</div>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
