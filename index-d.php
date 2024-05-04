3711700d2df99475dab661eb6d980b47bc2c61bb
Hafizasad07 <hafizasadrafique@gmail.com>
2024-05-04 18:21:09 +0500

    second update

--- index-d.php
+++ index-d.php
@@ -18,7 +18,7 @@
             </div>
         </div>
         <div class="raise-btn text-center py-5">
-            <a href=""><p class="title1">>> Raise My Scores <<</p><p class="font-14">Click Here to Unlock an 80% Discount Plus Free Bonuses</p><p class="font-12">(Limited-Time Offer)</p></a>
+            <a href=""><p class="font-size-27">>> Raise My Scores <<</p><p class="font-14">Click Here to Unlock an 80% Discount Plus Free Bonuses</p><p class="font-12">(Limited-Time Offer)</p></a>
         </div>
         <div class="raise-img text-center"><img src="assets/images/raise-img.png" alt="" class=""></div>
         <div class="container">
@@ -41,10 +41,83 @@
                 </ul>
             </div>
             <div class="raise-btn text-center pt-3 pb-5">
-                <a href=""><p class="title1">>> Raise My Scores <<</p><p class="font-14">Click Here to Unlock an 80% Discount Plus Free Bonuses</p><p class="font-12">(Limited-Time Offer)</p></a>
+                <a href=""><p class="font-size-27">>> Raise My Scores <<</p><p class="font-14">Click Here to Unlock an 80% Discount Plus Free Bonuses</p><p class="font-12">(Limited-Time Offer)</p></a>
             </div>
             <hr>
-            <div class="font-size-38 text-center pt-4">Plus, Get Instant Access To These FREE Bonuses…</div>
+            <div class="font-size-38 text-center pt-4 pb-4">Plus, Get Instant Access To These FREE Bonuses…</div>
+            <div class="section2">
+                <div class="row mx-auto w-100">
+                    <div class="col-lg-5 col-md-6 col-sm-6 col-12">
+                        <div class="quick-start text-center"><img src="assets/images/credit-secrets.png" alt=""></div>
+                    </div>
+                    <div class="col-lg-7 col-md-6 col-sm-6 col-12 ps-0">
+                        <div class="quickstart-heading">Credit Secrets Quickstart <br> Guide</div>
+                        <div class="font-size-27 text-light pt-1">($39.95 Value)</div>
+                        <div class="quickstart-description">
+                            This QuickStart Guide could have you seeing <br> improvements before you even get to the main Credit <br> Secrets Program! <br>
+                            <p class="pt-4">This short but powerful guide will show you <br> step-by-step how to get started toward improving <br> your scores... in the simplest and easiest way <br> possible.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="credit-secrets-ul">
+                    <ul>
+                        <li class="fw-bold pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"> How ONE simple letter could raise your scores by 30-70 points! (You can <br> even download this letter right on your dashboard when you login.)</li>
+                        <li class="pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"> The best part? It’s so simple to do, you can finish it in one day and be on <br> track to raising your scores this year.</li>
+                        <li class="fw-bold pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"> Step-by-step instructions to help you customize your letter based on your <br> specific situation and send it out right away. (If you just follow these <br> steps,, you will be on your way to getting your first wins with the Credit <br> Secrets Program.)</li>
+                        <li><img src="assets/icons/Tick.svg" alt="" class="me-2"> And more!</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="section2 mt-5">
+                <div class="row mx-auto w-100">
+                    <div class="col-lg-5 col-md-6 col-sm-6 col-12">
+                        <div class="quick-start text-center"><img src="assets/images/credit-secrets1.png" alt=""></div>
+                    </div>
+                    <div class="col-lg-7 col-md-6 col-sm-6 col-12 ps-0">
+                        <div class="quickstart-heading">Credit Secrets Guide To <br> Building Business Credit</div>
+                        <div class="font-size-27 text-light pt-1">($39.95 Value)</div>
+                        <div class="quickstart-description">
+                            If you've ever thought of starting your own home <br> business, the Credit Secrets Guide to Building <br> Business Credit can help you get there. 
+                            <p class="pt-4">You will discover how starting a business allows you <br>to secure business loans and lines of credit for doing <br>something you love.</p>
+                            <p class="pt-4">Here’s exactly what you’ll discover inside: </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="credit-secrets-ul">
+                    <ul>
+                        <li class="fw-bold pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"> Discover the ins and outs of getting approved for business credit and <br> secure the funding you need fast -- regardless if your business is <br> completely brand new.</li>
+                        <li class="pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"> Hear firsthand how one of our members secured over $260,000 in business <br> and personal credit with the help of Credit Secrets. (Feel free to take some <br>notes and apply the tips and strategies mentioned inside to get the funding <br> you need!)</li>
+                        <li class="fw-bold pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"> How to set up your new business correctly to maximize your chances of getting approved for funding</li>
+                        <li class="pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2">The inside scoop on applying for business bank accounts (including what to look for, the different accounts you need and an overlooked tool that can <br>help you find funding like loans and grants much faster)</li>
+                        <li class="fw-bold"><img src="assets/icons/Tick.svg" alt="" class="me-2"> And more!</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="section2 mt-5">
+                <div class="text-center">
+                    <div class="quickstart-heading">The Smart Money Club</div>
+                    <div class="font-size-38 text-light pt-1">($29.97 Value)</div>
+                </div>
+                <div class="smart-money text-center"><img src="assets/images/smart-money.png" alt="" class="w-100"></div>
+                <div class="font-size-20 pb-3 ps-5">Join our most successful members in the Smart Money Club private Facebook <br> group. </div>
+                <div class="font-size-20 pb-3 ps-5">Over 29,500 members are already inside, ready to help you maximize your success <br> with the program.</div>
+                <div class="font-size-20 pb-3 ps-5 fw-bold">Get all the help and support you need for a full two weeks absolutely free!</div>
+                <div class="font-size-20 pb-3 ps-5">Plus, you will get access to…</div>
+                <div class="credit-secrets-ul">
+                    <ul>
+                        <li class="pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"><div><span class="fw-bold">The John Ulzheimer Expert Files-</span> in this video training series, nationally renowned credit expert John teaches you advanced Credit Secrets that can improve your scores even more! (value $97)</div></li>
+                        <li class="pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"><div><span class="fw-bold">24/7 Group Support -</span> where you can ask questions… celebrate your successes… and listen in every credit-boosting secret shared in the group (value $497)</div></li>
+                        <li class="pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"><div><span class="fw-bold">The latest, Most Updated Credit Information and tricks</span>  relevant to current credit laws and regulations so you can have the best information within minutes of the time our sources share it with us (value $97)</div></li>
+                        <li class="pb-3"><img src="assets/icons/Tick.svg" alt="" class="me-2"><div><span class="fw-bold">Money-saving Hacks, Budgeting Tricks and Money Growth Challenges</span> to help you clear away debt faster than you ever imagined (value $67)</div></li>
+                        <li class="fw-bold"><img src="assets/icons/Tick.svg" alt="" class="me-2"> And more!</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="raise-btn text-center pt-5 pb-5">
+                <a href=""><p class="font-size-27">&gt;&gt; Raise My Scores &lt;&lt;</p><p class="font-14">Click Here to Unlock an 80% Discount Plus Free Bonuses</p><p class="font-12">(Limited-Time Offer)</p></a>
+            </div>
+            <hr class="mt-4">
+            <div class="font-size-38 text-center pt-5">Here’s How Our Members Have Used <br> Credit Secrets To Raise Their Scores & Transform <br>Their Financial Lives For The Better…</div>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
